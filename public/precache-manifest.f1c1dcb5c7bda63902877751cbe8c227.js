d7b986c41fd6b3c2e5ca43a940e1f97162428818
Boaz-coder <kemboiboaz42@gmail.com>
2023-08-07 22:30:30 +0300

    first commit

---
+++ public/precache-manifest.f1c1dcb5c7bda63902877751cbe8c227.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7828a3f059668723859603c0628102ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1732d2af8480c62f9140",
+    "url": "/static/css/3.fe63f49e.chunk.css"
+  },
+  {
+    "revision": "0b7fe395ae9479997fa1",
+    "url": "/static/css/4.66ae18a5.chunk.css"
+  },
+  {
+    "revision": "15b288fd00ca94b43328",
+    "url": "/static/css/5.fb1e8f66.chunk.css"
+  },
+  {
+    "revision": "7a30c96175b1462d08cc",
+    "url": "/static/css/6.db7e3b76.chunk.css"
+  },
+  {
+    "revision": "34aa6e689e9d4928af5d",
+    "url": "/static/css/7.39533c54.chunk.css"
+  },
+  {
+    "revision": "80195ad8bd2f8a6898bf",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "ac549acd5a05663bd33d",
+    "url": "/static/js/2.25314f50.chunk.js"
+  },
+  {
+    "revision": "1732d2af8480c62f9140",
+    "url": "/static/js/3.b14610f4.chunk.js"
+  },
+  {
+    "revision": "0b7fe395ae9479997fa1",
+    "url": "/static/js/4.3cf32105.chunk.js"
+  },
+  {
+    "revision": "15b288fd00ca94b43328",
+    "url": "/static/js/5.ffc221d5.chunk.js"
+  },
+  {
+    "revision": "7a30c96175b1462d08cc",
+    "url": "/static/js/6.950ba3e5.chunk.js"
+  },
+  {
+    "revision": "34aa6e689e9d4928af5d",
+    "url": "/static/js/7.e16d4b15.chunk.js"
+  },
+  {
+    "revision": "80195ad8bd2f8a6898bf",
+    "url": "/static/js/main.b507d24d.chunk.js"
+  },
+  {
+    "revision": "23dc3a23a3edb942c293",
+    "url": "/static/js/runtime-main.e7081479.js"
+  }
+]);
